b8e97ffe1fd9bca3a931ceab58ffce4ded04acf6
Alejandro <alejandrogallegos27@gmail.com>
2023-05-28 23:19:50 -0300

    tercer commit

--- src/auth/layout/AuthLayout.jsx
+++ src/auth/layout/AuthLayout.jsx
@@ -1,4 +1,5 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, } from '@mui/material';
+import { AccountCircleOutlined } from '@mui/icons-material';
 
 export const AuthLayout = ({ children, title = '' }) => {
   return (
@@ -7,6 +8,7 @@ export const AuthLayout = ({ children, title = '' }) => {
       spacing={ 0 }
       direction="column"
       alignItems="center"
+      textAlign="center"
       justifyContent="center"
       sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
 
@@ -18,9 +20,12 @@ export const AuthLayout = ({ children, title = '' }) => {
                 width: { md: 450 },
                 backgroundColor: 'white',
                 padding: 3,
-                borderRadius: 2 
-                }}>
+                borderRadius: 2
+          }}>
 
+          <Grid>
+            <AccountCircleOutlined sx={{ mb: 1 , fontSize: 60, }} />
+          </Grid>
           <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
 
             { children }
